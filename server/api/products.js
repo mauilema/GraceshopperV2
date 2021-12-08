2367920f0a6a58f6aeac9b6d1fcd07b2b19ed292
millyrocc <mili.conde24@yahoo.com>
2021-12-08 01:04:48 -0500

    completed feature for single product view

--- server/api/products.js
+++ server/api/products.js
@@ -1,14 +1,33 @@
-const router = require('express').Router()
-const { models: { Product }} = require('../db')
-module.exports = router
+const router = require('express').Router();
+const {
+  models: { Product },
+} = require('../db');
+module.exports = router;
+//OUTSTANDING:
+// - Need to create "Cart" & "Order" models in the db folder
+// - Need to import above mentioned modles on the top of this file
+// - Need to comment out the include on line 24
 
 router.get('/', async (req, res, next) => {
   try {
-    const AllProducts = await Product.findAll()
-    res.send(AllProducts)
+    const AllProducts = await Product.findAll();
+    res.send(AllProducts);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
-module.exports = router
+//get a single product:
+router.get('/:productId', async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.productId, {
+      //include: { Cart, Order },
+      //Getting an error stating that Cart & Order are not defined
+    });
+    res.send(product);
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
