597b0802ad8486cf066eb3bc5fe3b3ee01df0391
millyrocc <mili.conde24@yahoo.com>
2021-12-08 17:05:42 -0500

    added routes that redirected the user from all products to  a single product

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -1,43 +1,49 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { fetchProducts } from '../store/products'
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchProducts } from '../store/products';
+import { Link } from 'react-router-dom';
 
 class AllProducts extends React.Component {
-    componentDidMount () {
-        this.props.getProducts()
-    }   
-    render () {
-        console.log(this.props)
-        const { products } = this.props
-        return (
+  componentDidMount() {
+    this.props.getProducts();
+  }
+  render() {
+    console.log(this.props);
+    const { products } = this.props;
+    return (
+      <div>
+        <h1>Our Current Liquor Selection:</h1>
         <div>
-            <h1>Our Current Liquor Selection:</h1>
-            <div>
-                {products.length < 1 ? <h1>We are completely out of stock :(</h1> :
-                products.map(product => (
-                    <div className="single-product-border" key={product.id} >
-                        <h2>{product.name}</h2>
-                        <img src={product.image} />
-                        <h3>${product.price / 100}</h3>
-                    </div>
-                ))}
-            </div>
+          {products.length < 1 ? (
+            <h1>We are completely out of stock :(</h1>
+          ) : (
+            products.map((product) => (
+              <div className="single-product-border" key={product.id}>
+                <Link to={`/products/${product.id}`}>
+                  <h2>{product.name}</h2>
+                  <img src={product.image} />
+                  <h3>${product.price / 100}</h3>
+                </Link>
+              </div>
+            ))
+          )}
         </div>
-        )
-    }
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
-    return {
-        products: state.products
-    }
-}
+  return {
+    products: state.products,
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        getProducts: () => {
-            dispatch(fetchProducts())
-        }
-    }
-}
-export default connect(mapStateToProps, mapDispatchToProps)(AllProducts)
+  return {
+    getProducts: () => {
+      dispatch(fetchProducts());
+    },
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(AllProducts);
