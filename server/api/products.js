370db334f2b6b9dd0e367ff0a4b71faf0a3a10a7
mauilema <srjorgemaciel2002@gmail.com>
2021-12-14 23:57:41 -0500

    added an express route to get a single product by admin only through a authentication middleware with a token

--- server/api/products.js
+++ server/api/products.js
@@ -4,11 +4,9 @@ const {
   models: { Product },
 } = require("../db");
 module.exports = router;
-//OUTSTANDING:
-// - Need to create "Cart" & "Order" models in the db folder
-// - Need to import above mentioned modles on the top of this file
-// - Need to comment out the include on line 24
 
+
+//get all products; available to all users/guests
 router.get("/", async (req, res, next) => {
   try {
     const AllProducts = await Product.findAll();
@@ -34,8 +32,22 @@ router.get("/admin", verifyAdmin, async (req, res, next) => {
 router.get("/:productId", async (req, res, next) => {
   try {
     const product = await Product.findByPk(req.params.productId, {
-      //include: { Cart, Order },
-      //Getting an error stating that Cart & Order are not defined
+    });
+    if (!product) {
+      res.sendStatus(404)
+      return
+    }
+    res.send(product);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//get a single product admin only access with authentication middleware
+router.get("/admin/:productId", verifyAdmin, async (req, res, next) => {
+  try {
+
+    const product = await Product.findByPk(req.params.productId, {
     });
     if (!product) {
       res.sendStatus(404)
