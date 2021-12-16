fb7cbc23d1bf0cc4c3d34382eb7dbb1f3fe069e3
mauilema <srjorgemaciel2002@gmail.com>
2021-12-16 03:19:18 -0500

    rendering an edit product button that pops up an edit product form available to logged in admin only

--- client/components/SingleProductAdminView.js
+++ client/components/SingleProductAdminView.js
@@ -5,6 +5,20 @@ import { fetchProductAdmin } from '../store/singleProductAdmin';
 import EditProductByAdmin from './EditProductByAdmin';
 
 export class SingleProduct extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+        renderProductUserForm: false
+    }
+    this.handleEditButtonClick = this.handleEditButtonClick.bind(this)
+}
+
+handleEditButtonClick () {
+    this.setState({
+        renderProductUserForm: true
+    })
+}
+
   componentDidMount() {
     this.props.getProductAdmin(this.props.match.params.productAdminId);
   }
@@ -15,17 +29,18 @@ export class SingleProduct extends React.Component {
     return (
       <div>
         <div>
-          <div>
-            <EditProductByAdmin product={product}/>
-          </div>
           <div className="single-product-border">
             <h1>{product.name}</h1>
             <img className="products-image-size" src={product.image} />
             <h1>ABV: {product.ABV}%</h1>
             <h1>Stock Amount: {product.stockAmount}</h1>
             <h1>Price: ${product.price}</h1>
-            <p>Description: {product.description}</p>
+            <h3>Description: {product.description}</h3>
             <h3>Category: {product.alcoholType}</h3>
+            <div>
+                {this.state.renderProductUserForm && <EditProductByAdmin product={product}/>}
+                <button id='edit-user-restricted-button' onClick={() => this.handleEditButtonClick()} className="edit-button" type="submit">Edit This Product</button>
+            </div>
             <div className="back-to-all-products-link" >
             <Link to={'/adminProducts'}>
               Back to All Products
