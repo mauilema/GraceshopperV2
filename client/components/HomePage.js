cbbde1ccb62c00d4d4f7e35a0273fa6a260180ae
Christina Delancy <cdelancy11@yahoo.com>
2021-12-17 03:50:37 -0500

    home padme]

--- client/components/HomePage.js
+++ client/components/HomePage.js
@@ -16,20 +16,14 @@ export const HomePage = () => {
 	return (
 		<section className="home">
 			<div className="content">
-				<h3>should be our logo</h3>
-				<p>
-					Cincoro Tequilas are uniquely made with Weber blue agave from both the
-					highland and lowland regions that have been cooked and distilled
-					separately then artfully blended together into a single exceptional
-					tequila.
-				</p>
 				<img
 					onClick={sweetAlert()}
 					className="products-image-size"
-					src="https://image.shutterstock.com/image-photo/vavrisovo-slovakia-september-14-2019-600w-1573639624.jpg"
+					src="FullstackSpiritsLogo.jpeg"
 					height="50"
 					width="50"
 				/>
+				<Link to="/products">Shop All Liquors</Link>
 			</div>
 		</section>
 	);
